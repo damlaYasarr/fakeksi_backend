54482dd385fcd37ca7ec6ee267632bac48310a43
Damla YAŞAR <190315002@ogr.cbu.edu.tr>
2023-01-07 18:36:16 +0300

    Add project files.

---
+++ WEBAPI/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPI.Models;
+using WEBAPI.Services;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _usrservice;
+
+        public UserController(IUserService usrservice)
+        {
+            _usrservice = usrservice;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> GetAllHeroes()
+        {
+            return await _usrservice.GetAllUsers();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetSingleHero(int id)
+        {
+            
+
+            return Ok("");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Users>>> AddHero(Users hero)
+        {
+           // var result = await _superHeroService.AddHero(hero);
+            return Ok("");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Users>>> UpdateHero(int id, Users request)
+        {
+          //  var result = await _usrservice.UpdateHero(id, request);
+           
+
+            return Ok("");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Users>>> DeleteHero(int id)
+        {
+            var result = await _usrservice.DeleteUser(id);
+            if (result is null)
+                return NotFound("Hero not found.");
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WEBAPI/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace WEBAPI.Models
+{
+    public class Users
+    {
+
+        public int user_id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
