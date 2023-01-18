4cab393ca338df5ffca9ca749ac377635dd9ab21
Damla YAŞAR <190315002@ogr.cbu.edu.tr>
2023-01-18 22:36:41 +0300

    tag_class

---
+++ WEBAPI/Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace WEBAPI.Models
+{
+    public class Tag
+    {
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public string definition { get; set; }
+        public DateTime tags_date { get; set; }
+    }
+}
