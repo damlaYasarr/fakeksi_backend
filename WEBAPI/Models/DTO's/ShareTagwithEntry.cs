fe30107d60807ed142ba169d2848c32fdff48351
Damla YAŞAR <190315002@ogr.cbu.edu.tr>
2023-01-20 17:44:17 +0300

    trial

---
+++ WEBAPI/Models/DTO's/ShareTagwithEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace WEBAPI.Models.DTO_s
+{
+    public class ShareTagwithEntry
+    {
+        public int tag_id { get; set; }
+        public int user_id { get; set; }
+        public int entry_id { get; set; }
+    }
+}
--- WEBAPI/Services/TagEntryService.cs
+++ WEBAPI/Services/TagEntryService.cs
@@ -1,5 +1,6 @@
 ﻿using WEBAPI.Data;
 using WEBAPI.Models;
+using WEBAPI.Models.DTO_s;
 
 namespace WEBAPI.Services
 {
@@ -12,10 +13,42 @@ namespace WEBAPI.Services
             _context = context;
         }
         //başlık kısmı mutlaka entry ile paylaşılmalı
-        //mesela 
+       
         public async Task<Tag?> ShareTag(Tag tag)
         {
-           
+            /*
+              var result = from r in context.basvuru
+                                 join x in context.users on user_id equals x.Id
+                                 join u in context.status_table on r.status equals u.id into ux
+                                 from u in ux.DefaultIfEmpty()
+                                 where r.Id == user_id
+                                 select new ApplicationSchemaDto
+                                 {
+                                     id = r.Id,
+                                     user_id = r.User_Id,
+                                     Created = r.Zaman_Damgası,
+                                     status = u.id,
+                                     baslik = r.Baslik
+                                 };
+             */
+            var result = from r in _context.Tags
+                         join p in _context.Users on user_id equals r.
+                         join u in _context.Tags on r.tag_id equals u.id into ux
+                         from u in ux.DefaultIfEmpty()
+                         where r.Id == user_id
+                         select new ShareTagwithEntry
+                         {
+                             
+                         };
+
+
+            Tag tg = new Tag()
+            {
+                definition=tag.definition,
+                id=tag.id,
+                user_id=tag.user_id,
+
+            };
             _context.Tags.Add(tag);
             await _context.SaveChangesAsync();
             return tag;
