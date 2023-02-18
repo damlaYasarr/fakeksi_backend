edad91c98bf77dd28380880524683df6afaef306
damlaYasarr <damlayasar40@gmail.com>
2023-02-18 14:42:11 +0300

    jwt

---
+++ WEBAPI/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,10 @@
+﻿using WEBAPI.Models;
+
+namespace WEBAPI.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {    //json web token create with user info
+        AccessToken CreateToken(Users user, List<OperationClaim> operationclaims);
+
+    }
+}
