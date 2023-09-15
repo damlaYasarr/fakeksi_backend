43049f832c114af280a2023d392ebebfbd61ad22
damlaYasarr <damlayasar40@gmail.com>
2023-09-15 15:57:09 +0300

    emailconfig

--- WEBAPI/Services/emailconfig/emailconfig.py
+++ WEBAPI/Services/emailconfig/emailconfig.py
@@ -1,6 +1,19 @@
-print("ben python")
+import smtplib
+# import the smtplib module. It should be included in Python by default
 
-
+# set up the SMTP server _ after registration
+s = smtplib.SMTP(host='your_host_address_here', port=your_port_here)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
 
 def emailEntegration():
     pass
+def get_contacts(filename):
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
+
